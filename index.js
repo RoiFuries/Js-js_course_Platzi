66d8560a324f42a3b4f66d830baab8a9b15cd7fc
RoiFuries <roi.furies@gmail.com>
2020-07-29 10:59:46 -0500

    se creo el primer plugin y el boton de mute y unmute

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
-const video = document.querySelector("video")
-        const button = document.querySelector("button") // que pasa si en el html tengo mas de un video o un button
+import MediaPlayer from './MediaPlayer.js';
+import AutoPlay from './Plugins/AutoPlay.js';
 
-        function MediaPlayer(config){
-            this.media = config.el;
-        };
-
-        MediaPlayer.prototype.play = function (){
-            this.media.play();
-        };
-        MediaPlayer.prototype.pause = function (){
-            this.media.pause();
-        };
-
-        MediaPlayer.prototype.actionButton = function() {
-            if (this.media.paused) {
-             this.play();
-            } else {
-                 this.pause();
-            }
-      };
+const video = document.querySelector('video');
+const player = new MediaPlayer({ el: video, plugins:[
+    // new AutoPlay()
+] });
 
 
-        const player = new MediaPlayer({el: video});
-        button.onclick = () => player.actionButton();
+const button = document.querySelector("#playPause");
+button.onclick = () => player.togglePlay();
+
+const muteUnmute = document.querySelector('#unmuteMute');
+muteUnmute.onclick  =  ()  => player.unmuteMute();
+
+
+
+// const button = document.querySelector('button');
+// button.onclick = () => player.togglePlay();
+
